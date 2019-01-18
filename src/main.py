e03de4d7f61969874ae9a7333511b7036665214e
Blair Duncan <islay1885@gmail.com>
2019-01-18 17:25:36 +0000

    Added draw function, added glfw.swap_interval(0) to smooth gameplay

--- src/main.py
+++ src/main.py
@@ -56,18 +56,35 @@ def main():
             chip_8.keys[0x10] = action == glfw.PRESS
         return True
 
+    def draw():
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        for y in range(32):
+            for x in range(64):
+                if chip_8.display[x + (64 * y)] == 0:
+                    glColor3f(0.0, 0.0, 0.0)
+                else:
+                    glColor3f(1.0, 1.0, 1.0)
+                glBegin(GL_QUADS)
+                glVertex3f((x * MODIFIER), (y * MODIFIER), 0.0)
+                glVertex3f((x * MODIFIER), (y * MODIFIER) + MODIFIER, 0.0)
+                glVertex3f((x * MODIFIER) + MODIFIER, (y * MODIFIER) + MODIFIER, 0.0)
+                glVertex3f((x * MODIFIER) + MODIFIER, (y * MODIFIER) + 0.0, 0.0)
+                glEnd()
+
     if not glfw.init():
         return
 
     window = glfw.create_window(
         WIDTH * MODIFIER, HEIGHT * MODIFIER, "CHIP-8", None, None
     )
+
     if not window:
         glfw.terminate()
         return
 
     glfw.set_key_callback(window, key_callback)
     glfw.make_context_current(window)
+    glfw.swap_interval(0)
     gluOrtho2D(0, (WIDTH * MODIFIER), (HEIGHT * MODIFIER), 0)
 
     while not glfw.window_should_close(window):
@@ -75,19 +92,7 @@ def main():
         if not chip_8.emulate_cycle():
             break
         if chip_8.draw_flag == True:
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-            for y in range(32):
-                for x in range(64):
-                    if chip_8.display[x + (64 * y)] == 0:
-                        glColor3f(0.0, 0.0, 0.0)
-                    else:
-                        glColor3f(1.0, 1.0, 1.0)
-                    glBegin(GL_QUADS)
-                    glVertex3f((x * MODIFIER), (y * MODIFIER), 0.0)
-                    glVertex3f((x * MODIFIER), (y * MODIFIER) + MODIFIER, 0.0)
-                    glVertex3f((x * MODIFIER) + MODIFIER, (y * MODIFIER) + MODIFIER, 0.0)
-                    glVertex3f((x * MODIFIER) + MODIFIER, (y * MODIFIER) + 0.0, 0.0)
-                    glEnd()
+            draw()
             chip_8.draw_flag = False
         glfw.swap_buffers(window)
         glfw.poll_events()
