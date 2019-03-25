e62056e60b4c84707547fefb50843f6be1c6f507
Blair Duncan <blair.duncan@qnrl.com>
2019-03-25 17:13:25 +0000

    refactored test_chip8.py and added tests for jump_to_nnn and call_subroutine_at_nnn

--- test/test_chip8.py
+++ test/test_chip8.py
@@ -2,12 +2,9 @@ import pytest
 from src.chip8 import Chip8
 
 
-chip_8 = Chip8()
-opcode = 0x6A02
-
-
-# ensure the constructor initialises the values and array lengths correctly.
+# ensure the constructor initialises the variable values and array lengths correctly.
 def test_consutructor():
+    chip_8 = Chip8()
     assert chip_8.opcode == 0
     assert len(chip_8.memory) == 4096
     assert len(chip_8.v) == 16
@@ -24,8 +21,9 @@ def test_consutructor():
 
 # load the font set at index 0 to test the data load process.
 # the font set file contains the values noted in the Cowgod reference guide: http://devernay.free.fr/hacks/chip8/C8TECH10.HTM#2.4.
-# the remaining values for the memory should be zero, therefore we check that the memory is filled with non-zero values for index 0 to 79 and 0 values for the remainder of the memory
+# the remaining values for the memory should be zero, therefore we check that the memory is filled with non-zero values for index 0 to 79 and zero values for the remainder of the memory.
 def test_load_data():
+    chip_8 = Chip8()
     chip_8.load_data("font_set", 0)
 
     assert chip_8.memory[0] == 240
@@ -42,29 +40,40 @@ def test_load_data():
         assert chip_8.memory[j] == 0
         j += 1
 
-# tests for the bitwise operatorations.
+# tests for the bitwise operatorations using an opcode value of 27138.
 def test_nnn():
-    assert chip_8.nnn(opcode) == 0xA02
+    chip_8 = Chip8()
+    opcode = 27138
+    assert chip_8.nnn(opcode) == 2562
 
 
 def test_nn():
-    assert chip_8.nn(opcode) == 0x2
+    chip_8 = Chip8()
+    opcode = 27138
+    assert chip_8.nn(opcode) == 2
 
 
 def test_n():
-    assert chip_8.n(opcode) == 0x2
+    chip_8 = Chip8()
+    opcode = 27138
+    assert chip_8.n(opcode) == 2
 
 
 def test_x():
-    assert chip_8.x(opcode) == 0xA
+    chip_8 = Chip8()
+    opcode = 27138
+    assert chip_8.x(opcode) == 10
 
 
 def test_y():
-    assert chip_8.y(opcode) == 0x0
+    chip_8 = Chip8()
+    opcode = 27138
+    assert chip_8.y(opcode) == 0
 
 # fill all of the display elements with values of 1.
 # clear the screen and ensure all of the values have been reset to 0.
 def test_clear_screen():
+    chip_8 = Chip8()
     i = 0
     while i < len(chip_8.display):
         chip_8.display[i] += 1
@@ -80,12 +89,33 @@ def test_clear_screen():
         i += 1
 
 # set index 0 of the stack to 500.
-# set the stack pointer to 1 so that when return_from_subroutine is called the stackpointer should be decremented by one.
+# set the stack pointer to 1 so that when return_from_subroutine is called the stackpointer should be decremented by 1 to 0.
 # if the stack pointer is decremented by one then this should set the pc to the value of the first elemement of the stack, which we have set to 500.
 def test_return_from_subroutine():
+    chip_8 = Chip8()
     chip_8.stack[0] = 500
     chip_8.stack_pointer = 1
 
     chip_8.return_from_subroutine()
     assert chip_8.stack_pointer == 0
     assert chip_8.pc == 500
+
+
+# calling jump_to_nnn sets the pc to the return value of the bitwise nnn operation which takes an opcode as a parameter.
+def test_jump_to_nnn():
+    chip_8 = Chip8()
+    chip_8.opcode = 27138
+    chip_8.jump_to_nnn()
+    assert chip_8.pc == 2562
+
+
+# calling call_subroutine_at_nnn passes the stack pointer as an index to the stack, and sets this index to the value of the pc.
+# the stack pointer is then incremented by 1.
+# the pc is then set to the return value of the bitwise nnn operation which takes an opcode as a parameter.
+def test_call_subroutine_at_nnn():
+    chip_8 = Chip8()
+    chip_8.opcode = 27138
+    chip_8.call_subroutine_at_nnn()
+    assert chip_8.stack[0] == 512
+    assert chip_8.stack_pointer == 1
+    assert chip_8.pc == 2562
